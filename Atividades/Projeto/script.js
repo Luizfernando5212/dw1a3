c789ab35a1401d4ea91448c13bcbcdeac3f04717
lfoliveira <luiz.arruda@kiman.com.br>
2023-06-12 18:45:33 -0300

    localStorage

--- Atividades/Projeto/script.js
+++ Atividades/Projeto/script.js
@@ -1,3 +1,5 @@
+var datas = JSON.parse(localStorage.getItem("datas")) || [];
+
 function login() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
@@ -6,6 +8,23 @@ function login() {
         document.getElementById("loginPage").classList.add("hidden");
         document.getElementById("agendaPage").classList.remove("hidden");
         // Armazenar o estado do login, como um token de autenticação, em localStorage ou cookies
+        localStorage.setItem("isLoggedIn", true);
+
+        var entryList = document.getElementById("entryList");
+        datas.forEach(function (data) {
+            var newEntry = document.createElement("li");
+            newEntry.textContent = data;
+            
+
+            var deleteButton = document.createElement("button");
+            deleteButton.textContent = "Remover";
+            deleteButton.addEventListener("click", function () {
+                entryList.removeChild(newEntry);
+            });
+
+            newEntry.appendChild(deleteButton);
+            entryList.appendChild(newEntry);
+        });
     } else {
         alert("Credenciais inválidas. Tente novamente.");
     }
@@ -43,6 +62,9 @@ function addEntry() {
                 newEntry.appendChild(deleteButton);
                 entryList.appendChild(newEntry);
 
+                datas.push(data.result);
+                localStorage.setItem("datas", JSON.stringify(datas));
+
                 // Limpar o campo de input
                 entryInput.value = "";
             } else {
@@ -70,4 +92,34 @@ function logout() {
     document.getElementById("agendaPage").classList.add("hidden");
     document.getElementById("loginPage").classList.remove("hidden");
     // Limpar o estado do login, como o token de autenticação em localStorage ou cookies
+    localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("datas");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Verificar o estado de login ao carregar a página
+    var isLoggedIn = localStorage.getItem("isLoggedIn");
+
+    if (isLoggedIn) {
+        document.getElementById("loginPage").classList.add("hidden");
+        document.getElementById("agendaPage").classList.remove("hidden");
+    }
+
+    // Datas a serem carregadas
+    var entryList = document.getElementById("entryList");
+
+    datas.forEach(function (data) {
+        var newEntry = document.createElement("li");
+        newEntry.textContent = data;
+        
+
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Remover";
+        deleteButton.addEventListener("click", function () {
+            entryList.removeChild(newEntry);
+        });
+
+        newEntry.appendChild(deleteButton);
+        entryList.appendChild(newEntry);
+    });
+});
